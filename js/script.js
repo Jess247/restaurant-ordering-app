6948972ee4f48e81e8de089e3ed6ea9702b2ed9b
Jess247 <jesss8692@hotmail.com>
2024-02-09 13:37:37 +0100

    Add page render function for the object data

---
+++ js/script.js
@@ -0,0 +1,32 @@
+import {menuArray} from './data.js'
+const menuContainer = document.querySelector('#menu')
+menuContainer.addEventListener('click', (e) => {
+    if(e.target.dataset.btn) {
+        addItem(e.target.dataset.btn)
+    }
+} )
+
+function addItem(itemId) {
+    console.log(itemId)
+}
+
+function renderPage() {
+    const menuHtml = menuArray.map((menuItem) => 
+        `<div class="menu_item">
+            <div class="item_content">
+                <p class="icon">${menuItem.emoji}</p>
+                <div class="item_details">
+                    <h2 class="sub-heading">${menuItem.name}</h2>
+                    <p class="item_text">${menuItem.ingredients.join(', ')}</p>
+                    <p id="price">$ ${menuItem.price}</p>
+                </div>
+            </div>
+            <div class="item_btn" >
+                <i class="fa-solid fa-plus" data-btn="${menuItem.name}"></i>                        
+            </div>
+        </div>`).join('')
+    menuContainer.innerHTML = menuHtml
+}
+
+renderPage()
+
