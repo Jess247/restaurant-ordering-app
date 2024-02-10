9b543e25e82921638cf89be55f7944f0e0c94781
Jess247 <jesss8692@hotmail.com>
2024-02-10 13:57:19 +0100

    Add remove function to remove items from order

--- js/script.js
+++ js/script.js
@@ -3,9 +3,14 @@ const menuContainer = document.querySelector('#menu')
 const orderSection = document.querySelector('#order-section')
 const newItems = document.querySelector('#new-items')
 let totalPrice = 0
-menuContainer.addEventListener('click', (e) => {
+
+document.addEventListener('click', (e) => {
     if(e.target.dataset.btn) {
         addItem(e.target.dataset.btn)
+    } 
+    if(e.target.dataset.remove) {
+        // find out what the current item is and subtract from total
+        removeItem(e.target.dataset.remove, e.target.parentNode.parentNode)
     }
 } )
 
@@ -17,13 +22,19 @@ function addItem(itemId) {
     <div class="ordered-item" id="ordered-item">
         <div id="ordered-item-inner">
             <h4 >${currentItem.name}</h4>
-            <button class="remove-btn" id="remove-btn">remove</button>
+            <button class="remove-btn" id="remove-btn" data-remove="${currentItem.id}">remove</button>
         </div>
         <p id="ordered-item-price">$${currentItem.price}</p>
     </div>
     `
     document.querySelector('#total-price').textContent = `$ ${totalPrice}`
     console.log(totalPrice)
+}
+
+function removeItem(itemId, parentElement) {
+    totalPrice -= menuArray[itemId].price
+    document.querySelector('#total-price').textContent = `$ ${totalPrice}`
+    newItems.removeChild(parentElement)
 }
 
 function renderPage() {
