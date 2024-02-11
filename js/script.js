79949d6e2cd33dc4655c09f559308e090f3721d6
Jess247 <jesss8692@hotmail.com>
2024-02-11 13:34:39 +0100

    Add function to complete user order

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ import {menuArray} from './data.js'
 const menuContainer = document.querySelector('#menu')
 const orderSection = document.querySelector('#order-section')
 const newItems = document.querySelector('#new-items')
+const modal = document.querySelector('#modal')
+const form = document.querySelector('#form')
 let totalPrice = 0
 
 document.addEventListener('click', (e) => {
@@ -12,7 +14,13 @@ document.addEventListener('click', (e) => {
         // find out what the current item is and subtract from total
         removeItem(e.target.dataset.remove, e.target.parentNode.parentNode)
     }
+    if(e.target.id === 'complete-order-btn') {
+        orderSection.classList.add('hidden')
+        modal.classList.remove('hidden')
+    }
 } )
+
+form.addEventListener('submit',completeOrder)
 
 function addItem(itemId) {
     const currentItem = menuArray[itemId]
@@ -28,13 +36,30 @@ function addItem(itemId) {
     </div>
     `
     document.querySelector('#total-price').textContent = `$ ${totalPrice}`
-    console.log(totalPrice)
 }
 
 function removeItem(itemId, parentElement) {
     totalPrice -= menuArray[itemId].price
     document.querySelector('#total-price').textContent = `$ ${totalPrice}`
     newItems.removeChild(parentElement)
+}
+
+function completeOrder(e) {
+    e.preventDefault()
+    const inputField = document.querySelector('#name-input')
+    const name = inputField.value
+    modal.classList.add('hidden')
+    document.querySelector('#name').textContent = name
+    const successSection = document.querySelector('#success-section')
+    successSection.classList.remove('hidden')
+    clearInput()
+}
+
+function clearInput() {
+    const inputs = document.querySelectorAll('input')
+    inputs.forEach((input) => {
+        input.value = ''
+    })
 }
 
 function renderPage() {
